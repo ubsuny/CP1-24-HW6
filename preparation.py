70a991f669910564d3c82ad990cba5e947520ff1
Tolani4 <89810884+Tolani4@users.noreply.github.com>
2024-11-11 17:06:12 +0000

    finished tests

--- preparation.py
+++ preparation.py
@@ -1,5 +1,29 @@
-""" dummy preparations file """
+# preparations file 
 
-def dummy():
-    """ dummy functions for template file"""
-    return 0
+import numpy as np
+import pandas as pd
+
+def resize_to_fft_size(image_data):
+    """
+    Resize the input image (2D array) to the nearest power of two dimensions.
+    
+    Args:
+        image_data (pd.DataFrame): The input image data as a pandas DataFrame.
+        
+    Returns:
+        pd.DataFrame: The resized image data with dimensions as powers of two.
+    """
+    # Get current image dimensions
+    rows, cols = image_data.shape
+    
+    # Find the nearest power of two for rows and columns
+    new_rows = 2**int(np.floor(np.log2(rows)))
+    new_cols = 2**int(np.floor(np.log2(cols)))
+    
+    # Resize the image (either crop or pad the data)
+    if new_rows < rows and new_cols < cols:
+        resized_image = image_data.iloc[:new_rows, :new_cols]
+    else:
+        resized_image = image_data.reindex(index=range(new_rows), columns=range(new_cols), fill_value=0)
+    
+    return resized_image
--- test_preparation.py
+++ test_preparation.py
@@ -1,7 +1,18 @@
 """ unit test template """
 
-import preparation as prep
+import pytest
+import numpy as np
+import pandas as pd
+from preparation import resize_to_fft_size
 
-def test_dummy():
-    """ unit test for dummy function """
-    assert prep.dummy() == 0
+def test_resize_to_fft_size():
+    # Create a random 2D pandas DataFrame to simulate an image
+    image = np.random.rand(500, 300)
+    image_df = pd.DataFrame(image)
+
+    # Resize image
+    resized_image = resize_to_fft_size(image_df)
+
+    # Check if the dimensions are powers of two
+    assert (resized_image.shape[0] & (resized_image.shape[0] - 1)) == 0  # Rows should be power of 2
+    assert (resized_image.shape[1] & (resized_image.shape[1] - 1)) == 0  # Columns should be power of 2
